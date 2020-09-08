06ec01738de73b5696b1e277cf8a47120b8d6bb0
Bluerain <41102800+gasd238@users.noreply.github.com>
2020-09-08 11:45:20 +0900

    api를 통해 속도를 빠르게함

--- main.py
+++ main.py
@@ -1,62 +1,59 @@
 #-*- coding:utf-8 -*-
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
 from bs4 import BeautifulSoup
-import time
 import json
 import os
 import datetime
+import requests
+import urllib
+from threading import Thread
+import re
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrdir = "./chromedriver.exe"
-driver = webdriver.Chrome(chrdir, chrome_options=chrome_options)# 
-driver.get("https://www.twitch.tv/dkwl025/videos?filter=archives&sort=time")
-html = driver.find_element_by_css_selector("html")
-for _ in range(5):
-    time.sleep(2)
-    html.send_keys(Keys.END)
+with open('config.json') as json_file:
+    json_data = json.load(json_file)
+    client_id = json_data["client_id"]
+    token = json_data["token"]
 
-soup=BeautifulSoup(driver.page_source, 'lxml')
-driver.quit()
+def get_StreamerId(query):
+    headers = {'client-id' : client_id, 'Authorization':token}
+    page = requests.get("https://api.twitch.tv/helix/search/channels?query={}".format(query), headers=headers)
+    soup=BeautifulSoup(page.text, 'lxml')
+    return json.loads(soup.select('p')[0].text)['data'][0]['id']
 
-print("창 종료")
+def get_Video(streamerID):
+    headers = {'client-id' : client_id, 'Authorization':token}
+    page = requests.get("https://api.twitch.tv/helix/videos?user_id={}&first=100".format(str(streamerID)), headers=headers)
+    soup=BeautifulSoup(page.text, 'lxml')
+    return json.loads(soup.select('p')[0].text)['data']
 
-title = soup.find_all('h3', class_="tw-ellipsis tw-font-size-5")
-for i in range(len(title)):
-    title[i] = title[i].text
-img = soup.select("div.preview-card-thumbnail__image img")
-streamDate = []
-m3u8Src = []
-link = {}
-for i in img:
-    src = str(i['src'])
-    m3u8Src.append("https://vod-secure.twitch.tv/"+src.replace('https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/', "").replace('//thumb/thumb0-320x180.jpg', "")+"/chunked/index-dvr.m3u8")
-    streamDate.append(i['title'])
+def get_VideoUrl(query):
+    streamerID = get_StreamerId(query)
+    videos = get_Video(streamerID)
+    streamDate = []
+    m3u8Src = []
+    link = {}
+    title = []
+    for video in videos:
+        title.append(video['title'])
+        img = video['thumbnail_url']
+        m3u8Src.append("https://vod-secure.twitch.tv/"+img.replace('https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/', "").replace('//thumb/thumb0-%{width}x%{height}.jpg', "")+"/chunked/index-dvr.m3u8")
+        streamDate.append(re.search(r'(\d+-\d+-\d+)', video['created_at']).group())
 
-for i in range(len(title)):
-    link[streamDate[i]] = {}
-    link[streamDate[i]]['title'] = title[i]
-    link[streamDate[i]]['link'] = m3u8Src[i]
+    return m3u8Src, title, streamDate
 
-print("영상이 총 {}개 검색 되었습니다. 아래 목록에서 영상을 골라주세요".format(len(title)+1))
-for i in range(len(title)):
-    print(str(i+1)+". " + "{} 방송".format(streamDate[i]))
-    if i%3 == 0 and i != 0:
-        print('\n')
-
-select = int(input(">")) - 1
-print(m3u8Src[select])
-# title = streamDate[select] + " " + title[select]
-# title = title.rstrip().replace(" ", "_")
-
-# dir_ = input("다운받을 폴더 위치 > ")
-
-# print("youtube-dl -o {}/{}.mp4 {}".format(dir_, title, m3u8Src[select]))
-
-# if os.path.isdir(dir_):
-#     os.system("youtube-dl -o {}{}.mp4 {}".format(dir_, title, m3u8Src[select]))
 now = datetime.datetime.now()
-with open('./'+str(now.month)+str(now.day)+'.json', 'w', encoding='utf-8') as make_file:
-    json.dump(link, make_file, indent="\t", ensure_ascii=False)
+
+query = input("검색할 스트리머 > ")
+m3u8Src, title, streamDate = get_VideoUrl(query)
+print("영상이 총 {}개 검색 되었습니다. 아래 목록에서 영상을 골라주세요".format(len(title)))
+for i in range(len(title)):
+    print(str(i+1)+". " + "{} {} 방송".format(streamDate[i], title[i]))
+while True:
+    select = input("번호 입력 > ")
+    try:
+        select = int(select)-1
+        break
+    except:
+        continue
+title = re.search(r'(\d+-\d+-\d+)', streamDate[select]).group() + " " + title[select]
+title = title.rstrip().replace(" ", "_")
+print(title+"\n"+m3u8Src[select])
